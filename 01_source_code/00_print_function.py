b09f968f22fd7d8f53cba95f534191608b94ec2f
Muhammad Osama <imosamamumtaz@outlook.com>
2024-08-07 01:59:04 +0500

    print function code
    source code study

---
+++ 01_source_code/00_print_function.py
@@ -0,0 +1,37 @@
+# 1. Basic Printing - [Assignment 01]
+print("Hello World!")   #print Hello World!
+print("Muhammad")   #print first name
+
+# 2. Printing Multiple Items:
+print("Muhammad"+","+"Osama")   #print full name
+print(1,2,3)    #print 1,2,3
+
+# 3. Printing Special Characters:
+print("Hello,\nWorld!") # '\n' is a special character used as instruction in output to go on next line
+print("Hello,\tWorld!")    # '\t' is a special character used for tab indentation in output
+
+# 4. Using the sep parameter
+print('Apple','Banana','Cherry', sep=', ')
+print(1,2,3, sep='-')
+print('imosamamumtaz','gmail.com', sep='@')
+
+# 5. Using the end parameter☺``
+print('Hello', end=' ')
+print('World!')
+print(1, end='')
+print(2)
+
+# 6. Escape Characters
+print("He Said, \"Hello!\"")
+print("This is a backslash: \\")
+
+# 7. Combining Text and Numbers
+print("I am 20 years old")
+print("The number is", 5)
+
+# 8. Basic Loop for Printing
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
